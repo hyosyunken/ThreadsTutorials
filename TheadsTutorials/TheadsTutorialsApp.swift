547e3538bece4f8d41bb38f55dc2b1bf5a4bb46c
ronnie <ronnie147s@gmail.com>
2024-09-27 13:30:00 +0900

    Initial Commit

---
+++ TheadsTutorials/TheadsTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheadsTutorialsApp.swift
+//  TheadsTutorials
+//
+//  Created by ronnie on 2024/9/27.
+//
+
+import SwiftUI
+
+@main
+struct TheadsTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
